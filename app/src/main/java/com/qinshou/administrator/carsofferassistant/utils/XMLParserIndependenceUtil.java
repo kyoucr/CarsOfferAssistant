c46d551e6500154893aee901ef2a74adfcb40899
BeiYueMoTian <781467179@qq.com>
2016-07-07 13:46:55 +0800

    自助选车fragment

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/utils/XMLParserIndependenceUtil.java
@@ -0,0 +1,42 @@
+package com.qinshou.administrator.carsofferassistant.utils;
+
+import com.qinshou.administrator.carsofferassistant.bean.MyDefaultHandler;
+import com.qinshou.administrator.carsofferassistant.bean.Series;
+
+import org.xml.sax.SAXException;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+/**
+ * Created by zyj on 2016/7/6.
+ */
+
+public class XMLParserIndependenceUtil {
+    public static List<Series> parserIndependence(InputStream is) {
+        // 步骤：
+        // ①解析器工厂实例的构建
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        // ③解析
+        MyDefaultHandler handler = new MyDefaultHandler();
+        try {
+            // ②解析器
+            SAXParser parser = factory.newSAXParser();
+
+            parser.parse(is, handler);
+
+            // ④处理解析后的结果
+
+        } catch (ParserConfigurationException | SAXException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return handler.getSeriesList();
+    }
+}
