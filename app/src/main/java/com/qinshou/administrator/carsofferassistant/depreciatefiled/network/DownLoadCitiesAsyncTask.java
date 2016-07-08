ef3d44cded88d3ffd59b42dd16a3597a6e106695
yan717 <1371717874@qq.com>
2016-07-08 10:43:59 +0800

    DownLoadCitiesAsyncTask纠正

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/network/DownLoadCitiesAsyncTask.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/network/DownLoadCitiesAsyncTask.java
@@ -2,9 +2,22 @@ package com.qinshou.administrator.carsofferassistant.depreciatefiled.network;
 
 import android.os.AsyncTask;
 
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.CitiesListInfoBean;
+
 /**
- * Created by Administrator on 2016/7/8.
+ * Created by 岩 on 2016/7/8.
+ * 下载城市列表信息的异步任务
  */
 
-public class DownLoadCitiesAsyncTask extends AsyncTask {
+public class DownLoadCitiesAsyncTask extends AsyncTask<String,Void,CitiesListInfoBean> {
+
+    @Override
+    protected CitiesListInfoBean doInBackground(String... params) {
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(CitiesListInfoBean citiesListInfoBean) {
+        super.onPostExecute(citiesListInfoBean);
+    }
 }
