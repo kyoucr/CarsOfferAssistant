9f98199de2e8a62543a53c05fcb6d0d303c9e642
yan717 <1371717874@qq.com>
2016-07-08 11:53:32 +0800

    李光岩上传，下载城市信息的接口回调接口

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/inter/DownloadCitiesCallbackInter.java
@@ -0,0 +1,13 @@
+package com.qinshou.administrator.carsofferassistant.depreciatefiled.inter;
+
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.CitiesListInfoBean;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+
+public interface DownloadCitiesCallbackInter {
+
+    void hasDownLoadCities(CitiesListInfoBean citiesListInfoBean);
+
+}
