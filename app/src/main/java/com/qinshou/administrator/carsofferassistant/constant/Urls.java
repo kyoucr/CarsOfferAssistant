c13baeb8618a5530bf84bc61e1610f20730cfd46
yan717 <1371717874@qq.com>
2016-07-08 15:46:09 +0800

    李光岩上传，添加自己使用的城市列表的url

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/constant/Urls.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/constant/Urls.java
@@ -13,4 +13,11 @@ public class Urls {
      * 郑永杰
      */
     public static final String CAR_AUTO_SELECT = "http://app.cheyooh.com/i.ashx?m=car_auto_select&uid=dd122ac1318643b993baa7f52a61c245&location_cityid=3&ver=1.1.3&channel=P008%E8%B1%8C%E8%B1%86%E8%8D%9Av1.1.3&key=b5c2d857f489d912f438d8e7bc5ec75b&tagversion=va&appsku=andr_carprice&checkKey=baba4325da61583799415e34422ed5e4&pageEnter=2&&minPrice={0}&maxPrice={1}&compartment={2}&country=(3)&pageIndex={4}";
+
+
+    /**
+     * 李光岩
+     */
+    public static final String CITY_lIST_SELECT = "http://app.cheyooh.com/i.ashx?m=car_city&uid=dd122ac1318643b993baa7f52a61c245&location_cityid=3&ver=1.1.3&channel=P008%E8%B1%8C%E8%B1%86%E8%8D%9Av1.1.3&key=b5c2d857f489d912f438d8e7bc5ec75b&tagversion=va&appsku=andr_carprice&checkKey=baba4325da61583799415e34422ed5e4&pageEnter=3&timestamp=0";
+
 }
