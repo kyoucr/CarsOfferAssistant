7bce98707a4b961b802d6595656a20121b5228b5
BeiYueMoTian <781467179@qq.com>
2016-07-09 14:18:46 +0800

    接口追加。

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/inter/ImageShowCallback.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/inter/ImageShowCallback.java
@@ -1,16 +1,13 @@
 package com.qinshou.administrator.carsofferassistant.inter;
 
-import android.graphics.Bitmap;
-
 import com.qinshou.administrator.carsofferassistant.bean.Series;
 
 import java.util.List;
-import java.util.Map;
 
 /**
  * Created by zyj on 2016/7/6.
  */
 
-public interface IndependenceDataCallback {
-    void callBack(List<Series> seriesList);
+public interface ImageShowCallback {
+    void callBack(List<String> result);
 }
