e336fe3169f5e0042355c7dfe1674871ea215288
BeiYueMoTian <781467179@qq.com>
2016-07-11 14:33:54 +0800

    接口上传。

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/inter/TestInterface.java
@@ -0,0 +1,14 @@
+package com.qinshou.administrator.carsofferassistant.depreciatefiled.inter;
+
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.CitiesListInfoBean;
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.DealerListBean;
+
+/**
+ * Created by Administrator on 2016/7/8.
+ */
+
+public interface TestInterface {
+
+    void textCallBack(DealerListBean dealerListBean);
+
+}
