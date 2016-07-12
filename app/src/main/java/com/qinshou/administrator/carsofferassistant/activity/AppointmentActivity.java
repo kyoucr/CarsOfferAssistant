819c3fc8df4b0b77b3ea3beee8bfdecaa3bdfe7a
BeiYueMoTian <781467179@qq.com>
2016-07-12 16:15:52 +0800

    实现

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/activity/AppointmentActivity.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/activity/AppointmentActivity.java
@@ -1,15 +1,22 @@
 package com.qinshou.administrator.carsofferassistant.activity;
 
 import android.content.Intent;
+import android.database.SQLException;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.qinshou.administrator.carsofferassistant.R;
+import com.qinshou.administrator.carsofferassistant.bean.Customer;
+import com.qinshou.administrator.carsofferassistant.utils.DBHelper;
 
 /**
  * Created by 禽兽先生 on 2016.03.06
@@ -32,5 +39,40 @@ public class AppointmentActivity extends AppCompatActivity {
         Glide.with(this).load(car_picture).into(appointment_car_iv);
         appointment_serial_name_tv.setText(car_name_string);
         appointment_car_name_tv.setText(car_name);
+    }
+
+    /**
+     * 保存客户提交的数据
+     *
+     * @param view
+     */
+    public void saveCustomerData(View view) {
+        EditText customer_name_et = (EditText) findViewById(R.id.customer_name_et);
+        EditText customer_phone_et = (EditText) findViewById(R.id.customer_phone_et);
+        RadioGroup rg_gender_id = (RadioGroup) findViewById(R.id.rg_gender_id);
+        TextView show_city_tv = (TextView) findViewById(R.id.show_city_tv);
+
+        String name = customer_name_et.getText().toString();
+        String phone = customer_phone_et.getText().toString();
+        int checkedRadioButtonId = rg_gender_id.getCheckedRadioButtonId();
+        String gender = null;
+        if (checkedRadioButtonId == R.id.rb_male_id) {
+            gender = "男";
+        } else {
+            gender = "女";
+        }
+        String city = (String) show_city_tv.getText();
+        Customer customer = new Customer(name, phone, gender, city);
+        DBHelper helper = new DBHelper(this, "customer.db", null, 1);
+        SQLiteDatabase database = helper.getWritableDatabase();
+        try {
+            database.execSQL("insert into tb_customer(name,phone,gender,city) values(?,?,?,?)",
+                    new Object[]{customer.getName(), customer.getPhone(), customer.getGender(), customer.getCity()});
+            Toast.makeText(this, "提交成功！", Toast.LENGTH_LONG).show();
+            finish();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "提交失败！", Toast.LENGTH_LONG).show();
+        }
     }
 }
