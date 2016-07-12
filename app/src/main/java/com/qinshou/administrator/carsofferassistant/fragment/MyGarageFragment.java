eb65729c3e81c05ebfed54ffe727c513407486bc
yan717 <1371717874@qq.com>
2016-07-12 12:08:14 +0800

    wo de cheku

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/fragment/MyGarageFragment.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/fragment/MyGarageFragment.java
@@ -3,6 +3,11 @@ package com.qinshou.administrator.carsofferassistant.fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import com.qinshou.administrator.carsofferassistant.R;
 
 /**
  * Created by 禽兽先生 on 2016.07.05
@@ -13,4 +18,22 @@ public class MyGarageFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        aboutActionBar();
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    /**
+     * 关于ActionBar的操作
+     */
+    private void aboutActionBar(){
+        AppCompatActivity activity = ((AppCompatActivity) getActivity());
+        Toolbar toolbar = (Toolbar) activity.findViewById(R.id.main_tb);
+        activity.setSupportActionBar(toolbar);
+        ActionBar actionBar = activity.getSupportActionBar();
+        actionBar.setTitle("我的车库");
+    }
+
 }
