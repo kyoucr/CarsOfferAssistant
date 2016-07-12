2bec054a429457fca79a6e7551725d8c9367114f
yan717 <1371717874@qq.com>
2016-07-12 19:09:03 +0800

    wo de cheku

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/fragment/TabDetalFragment.java
@@ -0,0 +1,10 @@
+package com.qinshou.administrator.carsofferassistant.depreciatefiled.fragment;
+
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Administrator on 2016/7/12.
+ */
+
+public class TabDetalFragment extends FragmentStatePagerAdapter {
+}
