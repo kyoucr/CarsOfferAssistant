991523cceff8e3770e767058f48bfe7bf44ec0a0
BeiYueMoTian <781467179@qq.com>
2016-07-12 14:55:27 +0800

    实现

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/activity/AppointmentActivity.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/activity/AppointmentActivity.java
@@ -19,19 +19,28 @@ public class AppointmentActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_appointment);
-        Intent intent = getIntent();
-        String pic = intent.getStringExtra("pic");
-        String name = intent.getStringExtra("name");
-        String carName = intent.getStringExtra("carName");
-
         //1.找到界面上控件的实例
         ImageView appointment_car_iv = (ImageView) findViewById(R.id.appointment_car_iv);
         TextView appointment_serial_name_tv = (TextView) findViewById(R.id.appointment_serial_name_tv);
         TextView appointment_car_name_tv = (TextView) findViewById(R.id.appointment_car_name_tv);
 
-        //2.将数据设置到控件中。
-        Glide.with(this).load(pic).into(appointment_car_iv);
-        appointment_serial_name_tv.setText(name);
-        appointment_car_name_tv.setText(carName);
+        Intent intent = getIntent();
+        if (intent.getBooleanExtra("PAGE_CHOICE_FLG", false)) {
+            String car_name_string = intent.getStringExtra("car_name_string");
+            String car_name = intent.getStringExtra("car_name");
+            String car_picture_id = intent.getStringExtra("car_picture_id");
+            //2.将数据设置到控件中。
+            Glide.with(this).load(car_picture_id).into(appointment_car_iv);
+            appointment_serial_name_tv.setText(car_name_string);
+            appointment_car_name_tv.setText(car_name);
+        } else {
+            String pic = intent.getStringExtra("pic");
+            String name = intent.getStringExtra("name");
+            String carName = intent.getStringExtra("carName");
+            //2.将数据设置到控件中。
+            Glide.with(this).load(pic).into(appointment_car_iv);
+            appointment_serial_name_tv.setText(name);
+            appointment_car_name_tv.setText(carName);
+        }
     }
 }
