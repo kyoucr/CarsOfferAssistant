d51076ddf090ba206fdb9a914b2220bf337f6a89
BeiYueMoTian <781467179@qq.com>
2016-07-12 11:07:05 +0800

    实现

--- app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/activity/ReducePriceDetailActivity.java
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/activity/ReducePriceDetailActivity.java
@@ -12,6 +12,7 @@ import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
 import com.qinshou.administrator.carsofferassistant.R;
+import com.qinshou.administrator.carsofferassistant.activity.AppointmentActivity;
 import com.qinshou.administrator.carsofferassistant.constant.Urls;
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.adapter.ReduceCarAdapter;
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.DealersBean;
@@ -19,6 +20,7 @@ import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.ListRed
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.ReduceCar;
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.inter.ReduceCarDataCallBack;
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.network.ReduceCarDetailAsyncTask;
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.util.GPSDealerAddress;
 import com.qinshou.administrator.carsofferassistant.depreciatefiled.util.ReduceDetailParserUtil;
 
 import org.w3c.dom.Text;
@@ -59,35 +61,46 @@ public class ReducePriceDetailActivity extends AppCompatActivity implements Redu
         tv_decreace_price_id_price_id = (TextView) findViewById(R.id.tv_decreace_price_id_price_id);
         tv_sale_range_id = (TextView) findViewById(R.id.tv_sale_range_id);
         tv_day_left_id = (TextView) findViewById(R.id.tv_day_left_id);
+        TextView tv_location_id = (TextView) findViewById(R.id.tv_location_id);
+
+        new GPSDealerAddress(getApplication(), null, null, tv_location_id);//GPS定位功能添加。
 
         lv_all_car_reduce_price_id = (ListView) findViewById(R.id.lv_all_car_reduce_price_id);
         TextView tv_emptyp_id = (TextView) findViewById(R.id.tv_emptyp_id);
         lv_all_car_reduce_price_id.setEmptyView(tv_emptyp_id);
 
-        String urlStr = Urls.REDUCE_CAR_DETAIL+carMessage.getId();
+        String urlStr = Urls.REDUCE_CAR_DETAIL + carMessage.getId();
         new ReduceCarDetailAsyncTask(this).execute(urlStr);
     }
 
     /**
-     * 点击按钮事件
+     * 点击定位
      *
      * @param view
      */
     public void jumpAction(View view) {
-        Intent intent = new Intent(this, GPSDealerAddressActivity.class);
-        startActivity(intent);
+        switch (view.getId()) {
+            case R.id.tv_location_id:
+                startActivity(new Intent(this, GPSDealerAddressActivity.class));
+                break;
+            case R.id.btn_ask_lowest_price_id:
+                startActivity(new Intent(this, AppointmentActivity.class));
+                break;
+            default:break;
+        }
+
     }
 
 
     @Override
     public void dataCallBack(ListReduceCar reduceCar) {
-        tv_car_name_id.setText(reduceCar.getCsName()+" "+reduceCar.getCarName()+" "+reduceCar.getCarYear()+"款");
+        tv_car_name_id.setText(reduceCar.getCsName() + " " + reduceCar.getCarName() + " " + reduceCar.getCarYear() + "款");
         Glide.with(this).load(reduceCar.getPic()).into(iv_car_pic_id);
         tv_promote_price_id.setText(reduceCar.getOriginal_price());
         tv_old_price_id.setText(reduceCar.getNew_price());
         tv_decreace_price_id_price_id.setText(reduceCar.getReduce());
         tv_sale_range_id.setText(reduceCar.getRange());
-        tv_day_left_id.setText("剩"+reduceCar.getLast_day()+"天");
+        tv_day_left_id.setText("剩" + reduceCar.getLast_day() + "天");
 
         //2.数据源
 
