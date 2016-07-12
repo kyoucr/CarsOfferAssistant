3fb5bd5150b9d54b2a70638afbd3d7b94d9dec63
BeiYueMoTian <781467179@qq.com>
2016-07-12 16:17:10 +0800

    实现

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/utils/DBHelper.java
@@ -0,0 +1,30 @@
+package com.qinshou.administrator.carsofferassistant.utils;
+
+import android.content.Context;
+import android.database.DatabaseErrorHandler;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by zyj on 2016/7/12.
+ */
+
+public class DBHelper extends SQLiteOpenHelper {
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    public DBHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version, DatabaseErrorHandler errorHandler) {
+        super(context, name, factory, version, errorHandler);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table tb_customer(_id  integer primary key autoincrement,name varchar(50) not null, phone varchar(150) not null,gender varchar(4),city varchar(40))");// 书写建表语句
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
