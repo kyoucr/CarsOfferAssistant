7b60e91409429ee50e5b455cbc3ffed2a878d1bd
BeiYueMoTian <781467179@qq.com>
2016-07-12 10:34:08 +0800

    实现

---
+++ app/src/main/java/com/qinshou/administrator/carsofferassistant/depreciatefiled/inter/ReduceCarDataCallBack.java
@@ -0,0 +1,11 @@
+package com.qinshou.administrator.carsofferassistant.depreciatefiled.inter;
+
+import com.qinshou.administrator.carsofferassistant.depreciatefiled.bean.ListReduceCar;
+
+/**
+ * Created by zyj on 2016/7/11.
+ */
+
+public interface ReduceCarDataCallBack {
+    void dataCallBack(ListReduceCar reduceCar);
+}
